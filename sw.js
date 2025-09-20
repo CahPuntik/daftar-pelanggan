0202afc75294debb64516a03676ef3c408d2ac47
Slamet Herianto <heri.fromrfive@gmail.com>
2025-09-20 20:31:07 +0800

    update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,49 @@
+const CACHE_NAME = "app-cache-" + new Date().getTime();
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+// Install Service Worker
 self.addEventListener("install", (event) => {
+  self.skipWaiting(); // langsung aktifkan SW baru
   event.waitUntil(
-    caches.open("app-cache").then((cache) => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json",
-        "./icon-192.png",
-        "./icon-512.png"
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
+// Activate Service Worker
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache); // hapus cache lama
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch dengan strategi network-first
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request)
+      .then((response) => {
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
